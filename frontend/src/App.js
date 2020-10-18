b8e539a6a3058689e3ea7e8706f1f378cacb806d
EthanYooBuzzvil <ethan.yoo@buzzvil.com>
2020-10-18 22:38:42 +0900

    chage uri pattern to relative path from absolute path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
   };
 
   loadItem = () => {
-    axios.get("http://127.0.0.1:8000/libraries/")
+    axios.get("/libraries/")
     .then(({data}) => {
       this.setState({
         libraries: data
