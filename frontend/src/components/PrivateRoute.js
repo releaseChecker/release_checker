f4fb360e2863989eb75571ba8a3057159200373b
EthanYooBuzzvil <ethan.yoo@buzzvil.com>
2020-10-25 09:35:48 +0900

    impl PrivateRoute

---
+++ frontend/src/components/PrivateRoute.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+
+class PrivateRoute extends Component {
+    render() {
+        const { component: PrivateComponent, ...rest } = this.props;
+        const isAuthenticated = true // TODO integrate with login
+
+        return (
+            <Route {...rest} render={props => (
+                isAuthenticated ? <PrivateComponent {...props} />
+                    : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+            )} />
+        )
+    }
+}
